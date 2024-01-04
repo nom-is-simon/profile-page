0d5eb085fdd8c32aa60238ce37a952acb2e37078
nom-is-simon <he.simon24@gmail.com>
2024-01-03 20:47:43 -0500

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+                TRVL <i className="fab f-typo3"
+            </Link>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
