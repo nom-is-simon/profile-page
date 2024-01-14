6dd7abe57ab8685d00d5aee51417e191659aea1e
Simon He <he.simon24@gmail.com>
2024-01-14 18:56:23 -0500

    completed tutorial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path='/'/>
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' Component={Home}/>
+          <Route path='/services' Component={Services} />
+          <Route path='/products' Component={Products} />
+          <Route path='/sign-up' Component={SignUp} />
+        </Routes>
+      </Router>
     </>
   );
 }
